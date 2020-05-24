7cc7b747e092977e75d9c57a6ab51a115f7c8e3a
RRRR <rafael.ramosr@hotmail.com>
2020-05-23 20:18:54 -0500

    Problem 1 - Comments added

--- archives/problems1to50/problem1.go
+++ archives/problems1to50/problem1.go
@@ -1,16 +1,15 @@
 package problems1to50
 
-/*Multiples of 3 and 5
-Description: https://projecteuler.net/problem=1
-*/
 import (
 	"fmt"
 )
 
+// Problem1 : Multiples of 3 and 5
+// Description: https://projecteuler.net/problem=1
 func Problem1() {
 	i := 1
 	result := 0
-
+	// All the multiples of 3 or 5 below 1000
 	for i <= 1000 {
 		if i%3 == 0 || i%5 == 0 {
 			result += i
